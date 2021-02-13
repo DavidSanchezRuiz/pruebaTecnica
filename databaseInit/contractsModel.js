c39b0d7b8bd4b481f12ed37ce5fe0e7427810fe0
David <davidignacio.sanchez@uptc.edu.co>
2021-02-13 13:06:18 -0500

    fix

--- databaseInit/contractsModel.js
+++ databaseInit/contractsModel.js
@@ -4,8 +4,8 @@ module.exports.create = function (db) {
     db.run(`CREATE TABLE contract (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(45) NOT NULL,
-        date DATE NOT NULL UNIQUE,
-        file VARCHAR(45) NOT NULL UNIQUE,
+        date DATE NOT NULL,
+        file VARCHAR(45) NOT NULL,
         employees_id BIGINT(20),
         FOREIGN KEY (employees_id) REFERENCES employees (employees_id) 
          ON DELETE CASCADE 
